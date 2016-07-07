698d5653af0913504215002ff4323902500438bd
kongr45gpen <electrovesta@gmail.com>
2016-07-07 13:40:52 +0300

    Update Symfony and fix deprecations

--- src/AppBundle/Security/BZDBAuthenticator.php
+++ src/AppBundle/Security/BZDBAuthenticator.php
@@ -5,20 +5,24 @@ namespace AppBundle\Security;
 use AppBundle\Entity\User;
 use Doctrine\ORM\EntityManager;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\Security\Core\Authentication\SimplePreAuthenticatorInterface;
-use Symfony\Component\Security\Core\Authentication\Token\PreAuthenticatedToken;
-use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\Routing\Router;
 use Symfony\Component\Security\Core\Exception\AuthenticationException;
 use Symfony\Component\Security\Core\Exception\BadCredentialsException;
+use Symfony\Component\Security\Core\User\UserInterface;
 use Symfony\Component\Security\Core\User\UserProviderInterface;
-use Symfony\Component\Security\Http\Authentication\AuthenticationFailureHandlerInterface;
+use Symfony\Component\Security\Guard\Authenticator\AbstractFormLoginAuthenticator;
 
-class BZDBAuthenticator implements SimplePreAuthenticatorInterface, AuthenticationFailureHandlerInterface
+class BZDBAuthenticator extends AbstractFormLoginAuthenticator
 {
     /**
      * @var EntityManager
      */
     protected $entityManager;
+
+    /**
+     * @var Router
+     */
+    protected $router;
 
     /**
      * The list of accepted BZFlag groups.
@@ -36,44 +40,43 @@ class BZDBAuthenticator implements SimplePreAuthenticatorInterface, Authenticati
      * Create new BZDBAuthenticator.
      *
      * @param EntityManager   $entityManager The doctrine entity manager
+     * @param Router          $router        The Symfony router
      * @param string          $debug         Whether the kernel is on debug mode
      * @param string[]|string $groups        The accepted BZFlag groups
      */
-    public function __construct(EntityManager $entityManager, $groups, $debug)
+    public function __construct(EntityManager $entityManager, Router $router, $groups, $debug)
     {
         $this->entityManager = $entityManager;
+        $this->router        = $router;
         $this->groups        = is_array($groups) ? $groups : array($groups);
         $this->debug         = $debug;
     }
 
-    public function createToken(Request $request, $providerKey)
+    /**
+     * Called on every request. Return whatever credentials you want,
+     * or null to stop authentication.
+     */
+    public function getCredentials(Request $request)
     {
         $username = $request->query->get('username');
         $token    = $request->query->get('token');
 
         if (!$username || !$token) {
-            throw new AuthenticationException('No authentication token found');
+            return null;
         }
 
-        return new PreAuthenticatedToken(
-            'anon.',
-            array($username, $token),
-            $providerKey
-        );
+        return [
+            'username' => $username,
+            'token' => $token
+        ];
     }
 
-    public function authenticateToken(TokenInterface $token, UserProviderInterface $userProvider, $providerKey)
+    public function getUser($credentials, UserProviderInterface $userProvider)
     {
-        $credentials = $token->getCredentials();
-        $username    = $credentials[0];
-        $token       = $credentials[1];
-
-        $bzData = \validate_token($token, $username, $this->groups, !$this->debug);
+        $bzData = \validate_token($credentials['token'], $credentials['username'], $this->groups, !$this->debug);
 
         if (!$bzData) {
-            throw new BadCredentialsException(
-                'The authentication token provided is invalid'
-            );
+            throw new BadCredentialsException("The authentication token provided is invalid");
         }
 
         if ($this->groups && !empty($this->groups)) {
@@ -85,6 +88,8 @@ class BZDBAuthenticator implements SimplePreAuthenticatorInterface, Authenticati
             }
         }
 
+        // if null, authentication will fail
+        // if a User object, checkCredentials() is called
         $bzid = $bzData['bzid'];
         $user = $this->entityManager
             ->getRepository('AppBundle:User')
@@ -99,13 +104,16 @@ class BZDBAuthenticator implements SimplePreAuthenticatorInterface, Authenticati
         $this->entityManager->persist($user);
         $this->entityManager->flush();
 
-        // TODO: Custom token class
-        return new PreAuthenticatedToken(
-            $user,
-            $username,
-            $providerKey,
-            $user->getRoles()
-        );
+        return $user;
+    }
+
+    public function checkCredentials($credentials, UserInterface $user)
+    {
+        // check credentials - e.g. make sure the password is valid
+        // no credential check is needed in this case
+
+        // return true to cause authentication success
+        return true;
     }
 
     public function onAuthenticationFailure(Request $request, AuthenticationException $exception)
@@ -117,8 +125,33 @@ class BZDBAuthenticator implements SimplePreAuthenticatorInterface, Authenticati
         }
     }
 
-    public function supportsToken(TokenInterface $token, $providerKey)
+    public function supportsRememberMe()
     {
-        return $token instanceof PreAuthenticatedToken && $token->getProviderKey() === $providerKey;
+        return false;
     }
+
+    /**
+     * Return the URL to the login page.
+     *
+     * @return string
+     */
+    protected function getLoginUrl()
+    {
+        return $this->router->generate('login_route');
+    }
+
+    /**
+     * The user will be redirected to the secure page they originally tried
+     * to access. But if no such page exists (i.e. the user went to the
+     * login page directly), this returns the URL the user should be redirected
+     * to after logging in successfully (e.g. your homepage).
+     *
+     * @return string
+     */
+    protected function getDefaultSuccessRedirectUrl()
+    {
+        return $this->router->generate('survey');
+    }
+
+
 }

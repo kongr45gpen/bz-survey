b715d5e34aca14676126ca5dbb4ec4185ea09cb3
kongr45gpen <electrovesta@gmail.com>
2016-07-22 22:32:43 +0300

    Use natural sorting for usernames

--- src/AppBundle/Twig/AppExtension.php
+++ src/AppBundle/Twig/AppExtension.php
@@ -16,6 +16,15 @@ class AppExtension extends \Twig_Extension
                     yield $accessor->getValue($element, $attribute);
                 }
             }),
+            new \Twig_SimpleFilter('natsort', function($array) {
+                if (!is_array($array)) {
+                    $array = iterator_to_array($array);
+                }
+
+                natcasesort($array);
+
+                return $array;
+            }),
         ];
     }
 
